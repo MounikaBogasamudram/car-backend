f3bfbfbc308ece03c4ec741b14d45bf7fffff397
BogasamudramMounika <mounikaboga8@gmail.com>
2023-08-04 15:02:44 +0530

    first commit

---
+++ src/main/java/com/diet/api/UserRepo.java
@@ -0,0 +1,11 @@
+package com.diet.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+
+	User findByUserName(String userName);
+
+	
+
+}
